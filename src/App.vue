e1c50c969add71d08f1651e898a5fbd016d77cb0
nicolas garces <nicolasgarces0@gmail.com>
2022-11-30 17:07:35 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <Counter title="Entraron" /> <!--se define en el template-->
+  <Counter title="Salieron" /> 
+</template> 
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue' //se importa el componente creado
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { // se define en los components
+    Counter
+   }
+  
 }
 </script>
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,68 @@
+import Co from 'module';
+<template>
+  <h2>{{title || 'Counter'}}</h2>
+  <p>{{ counter }}<sup>2</sup> =  {{( counter * counter )}}</p> 
+
+    <div>
+        <button v-on:click="increase">+1</button>
+        <button v-on:click="decrease">-1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title'], 
+    data() {
+        return {
+           counter: 5 
+           
+        }
+    },
+    methods:{
+        getSquareValue() {
+            console.log('getSquareValue')
+            return this.counter * this.counter 
+        },
+        increase(){
+            console.log('increase');
+            this.counter++
+        },
+        decrease(){
+            console.log('decrease');
+            this.counter--
+        },
+        
+    },
+    computed:{ // las propiedades computadas se guardan en el cache
+        squareCounter (){
+            console.log('computed squareCounter')
+            return this.counter * this.counter 
+            
+        },
+        customTitle(){
+             
+        }
+        
+    }
+}
+</script>
+
+<style>
+
+button{
+    background-color: #64bb87;
+    border-radius: 5px;
+    border:  1px solid white;
+    color: white;
+    cursor: pointer;
+    margin : 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out
+}
+
+button:hover {
+    background-color: #5aa67b;
+    transition: 0.3s ease-in-out;
+}
+
+</style>
